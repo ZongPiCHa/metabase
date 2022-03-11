b1563bb075cdadf817bccf37ece102012426c0fa
Ngoc Khuat <qn.khuat@gmail.com>
2022-03-11 22:16:10 +0700

    fix failed to sync admin group

--- test/metabase/integrations/common_test.clj
+++ test/metabase/integrations/common_test.clj
@@ -53,7 +53,7 @@
   (testing "does syncing group memberships leave existing memberships in place if nothing has changed?"
     (with-user-in-groups [group {:name (str ::group)}
                           user  [group]]
-      (integrations.common/sync-group-memberships! user #{group} #{group} false)
+      (integrations.common/sync-group-memberships! user #{group} #{group})
       (is (= #{"All Users" ":metabase.integrations.common-test/group"}
              (group-memberships user)))))
 
@@ -64,7 +64,7 @@
                                                       :group_id (u/the-id group)
                                                       :user_id  (u/the-id user))
             original-membership-id (membership-id)]
-        (integrations.common/sync-group-memberships! user #{group} #{group} false)
+        (integrations.common/sync-group-memberships! user #{group} #{group})
         (is (= original-membership-id
                (membership-id))))))
 
@@ -72,7 +72,7 @@
     (with-user-in-groups [group-1 {:name (str ::group-1)}
                           group-2 {:name (str ::group-2)}
                           user    [group-1]]
-      (integrations.common/sync-group-memberships! user #{group-1 group-2} #{group-1 group-2} false)
+      (integrations.common/sync-group-memberships! user #{group-1 group-2} #{group-1 group-2})
       (is (= #{":metabase.integrations.common-test/group-1"
                ":metabase.integrations.common-test/group-2"
                "All Users"}
@@ -82,7 +82,7 @@
     (with-user-in-groups [group-1 {:name (str ::group-1)}
                           group-2 {:name (str ::group-2)}
                           user    [group-1]]
-      (integrations.common/sync-group-memberships! user #{} #{group-1 group-2} false)
+      (integrations.common/sync-group-memberships! user #{} #{group-1 group-2})
       (is (= #{"All Users"}
              (group-memberships user)))))
 
@@ -90,55 +90,42 @@
     (with-user-in-groups [group-1 {:name (str ::group-1)}
                           group-2 {:name (str ::group-2)}
                           user    [group-1]]
-      (integrations.common/sync-group-memberships! user #{group-2} #{group-1 group-2} false)
+      (integrations.common/sync-group-memberships! user #{group-2} #{group-1 group-2})
       (is (= #{":metabase.integrations.common-test/group-2" "All Users"}
              (group-memberships user)))))
 
   (testing "are unmapped groups ignored when adding group memberships?"
     (with-user-in-groups [group-1 {:name (str ::group-1)}
                           user    []]
-      (integrations.common/sync-group-memberships! user #{group-1} #{} false)
+      (integrations.common/sync-group-memberships! user #{group-1} #{})
       (is (= #{"All Users"} (group-memberships user)))))
 
-  (testing "are unmapped groups ignored when removing group memberships?"
+  ;; when user mapping group are enabled, user can only manage groups using authentication provider
+  (testing "are unmapped groups removed when removing group memberships?"
     (with-user-in-groups [group-1 {:name (str ::group-1)}
                           user    [group-1]]
-      (integrations.common/sync-group-memberships! user #{} #{} false)
-      (is (= #{":metabase.integrations.common-test/group-1"
-               "All Users"}
+      (integrations.common/sync-group-memberships! user #{} #{})
+      (is (= #{"All Users"}
              (group-memberships user)))))
 
   (testing "if we attempt to add a user to a group that doesn't exist, does the group sync complete for the other groups?"
     (with-user-in-groups [group {:name (str ::group)}
                           user    []]
       (tu.log/suppress-output
-       (integrations.common/sync-group-memberships! user #{Integer/MAX_VALUE group} #{Integer/MAX_VALUE group} false))
+       (integrations.common/sync-group-memberships! user #{Integer/MAX_VALUE group} #{Integer/MAX_VALUE group}))
       (is (= #{"All Users" ":metabase.integrations.common-test/group"}
              (group-memberships user)))))
 
   (mt/with-test-user :crowberto
-    (testing "with admin group sync enabled"
+    (testing "Admin should be synced just like a normal grup"
       (testing "are admins synced?"
         (with-user-in-groups [user]
-          (integrations.common/sync-group-memberships! user [(group/admin)] #{} true)
-          (is (= #{"Administrators" "All Users"}
+          (integrations.common/sync-group-memberships! user [(group/admin)] #{})
+          (is (= #{"All Users"}
                  (group-memberships user)))))
 
       (testing "are administrators removed appropriately?"
         (with-user-in-groups [user [(group/admin)]]
-          (integrations.common/sync-group-memberships! user [] #{} true)
+          (integrations.common/sync-group-memberships! user [] #{})
           (is (= #{"All Users"}
-                 (group-memberships user)))))))
-
-  (testing "with admin group sync disabled"
-    (testing "are admins synced?"
-      (with-user-in-groups [user]
-        (integrations.common/sync-group-memberships! user [(group/admin)] #{} false)
-        (is (= #{"All Users"}
-               (group-memberships user)))))
-
-    (testing "are administrators removed appropriately?"
-      (with-user-in-groups [user [(group/admin)]]
-        (integrations.common/sync-group-memberships! user [] #{} false)
-        (is (= #{"Administrators" "All Users"}
-               (group-memberships user)))))))
+                 (group-memberships user))))))))
